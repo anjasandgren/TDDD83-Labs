d3a20f6b7bf881dcf2a0b7db68ed81fc0dac0161
anjsa296 <anjsa296@su24-208.ad.liu.se>
2025-01-24 12:08:43 +0100

    Lab1 start

--- server/main.py
+++ server/main.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python3
 from flask import Flask
 from flask import jsonify
+from flask import abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return jsonify("Hello, World!")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+# class Car(db.Model):
+# id = db.Column(db.Integer, primary_key=True)
+# make = db.Column(db.String, nullable=False)
+# model = db.Column(db.String, nullable=False)
+
+# def __repr__(self):
+#     return '<Car {}: {} {}'.format(self.id, self.make, self.model) 
+
+car_list = [
+  {"id": 1, "make": "Volvo", "model": "V70"},
+  {"id": 2, "make": "SAAB", "model": "95"}
+]
+
+@app.route('/cars')
+def cars():
+    return jsonify(car_list)
+
+@app.route('/cars/<int:car_id>')
+def show_car_id(car_id):
+    if car_id <= len(car_list):
+        return jsonify(car_list[car_id-1])
+    else :
+        abort(404)
 
 if __name__ == "__main__":
-    app.run(port=5000) # På MacOS, byt till 5001 eller dylikt
+    app.run(port=5000, debug=True)
