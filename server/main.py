db083f4dfceb6d0cdbf77b46f948eb5dfbb6c2fd
anjsa296 <anjsa296@su24-208.ad.liu.se>
2025-01-28 10:17:24 +0100

    Labpass progress

--- server/main.py
+++ server/main.py
@@ -6,29 +6,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# class Car(db.Model):
-# id = db.Column(db.Integer, primary_key=True)
-# make = db.Column(db.String, nullable=False)
-# model = db.Column(db.String, nullable=False)
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String, nullable=False)
+    model = db.Column(db.String, nullable=False)
 
-# def __repr__(self):
-#     return '<Car {}: {} {}'.format(self.id, self.make, self.model) 
-
-car_list = [
-  {"id": 1, "make": "Volvo", "model": "V70"},
-  {"id": 2, "make": "SAAB", "model": "95"}
-]
+    def __repr__(self):
+        return '<Car {}: {} {}'.format(self.id, self.make, self.model)
 
 @app.route('/cars')
 def cars():
+    car_list = Car.query.all()
     return jsonify(car_list)
 
 @app.route('/cars/<int:car_id>')
 def show_car_id(car_id):
+    #car_list = Car.query.all()
     if car_id <= len(car_list):
         return jsonify(car_list[car_id-1])
     else :
