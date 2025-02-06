63bae30694a87647ab8cca3277521356d3537e46
anjsa296 <anjsa296@su04-207.ad.liu.se>
2025-02-06 12:00:17 +0100

    Del 1-4 done, next step is sessionStorage()

--- client/client.js
+++ client/client.js
@@ -4,7 +4,7 @@ function getHost() {
 }
 
 let car_list = [];
-let customer_list = [];
+let user_list = [];
 
 $(document).ready(function(){
    // alert("Sidan laddades");
@@ -15,8 +15,8 @@ $.get(getHost() + '/cars', function(cars) {
    car_list = cars;
 });
 
-$.get(getHost() + '/customers', function(customers) {
-   customer_list = customers;
+$.get(getHost() + '/users', function(users) {
+   user_list = users;
 });
 
 function getCar(id, callback) {
@@ -33,7 +33,7 @@ function addCar(added_car) {
       data: JSON.stringify({
          make: added_car.make,
          model: added_car.model,
-         customer: added_car.customer.id
+         user: added_car.user.id
       }),
       success: function(response) {
          console.log('Success:', response);
@@ -47,26 +47,26 @@ function addCar(added_car) {
    });
 }
 
-function data(make_upd, model_upd, customer_id_upd) {
+function data(make_upd, model_upd, user_id_upd) {
    return JSON.stringify({
       make: make_upd,
       model: model_upd,
-      customer_id: customer_id_upd
+      user_id: user_id_upd
    })
 }
 
-function updateCar(id_upd, make_upd, model_upd, customer_id_upd) {
+function updateCar(id_upd, make_upd, model_upd, user_id_upd) {
    $.ajax({
       url: getHost() + `/cars/${id_upd}`,
       type: 'PUT',
       contentType: 'application/json',
-      data: data(make_upd, model_upd, customer_id_upd),
+      data: data(make_upd, model_upd, user_id_upd),
       success: function(response) {
          console.log('Success:', response);
          let car_to_update = getCarFromId(id_upd);
          car_to_update.make = make_upd;
          car_to_update.model = model_upd;
-         car_to_update.customer = getCustomerFromId(customer_id_upd);
+         car_to_update.user = getUserFromId(user_id_upd);
 
          const carIndex = car_list.findIndex(car => car.id === id_upd);
          if (carIndex !== -1) {
@@ -105,6 +105,50 @@ function deleteCar(id) {
 }
 
 
+function signUp(new_user) {
+   $.ajax({
+      url: getHost() + '/sign-up',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+         name: new_user.name,
+         email: new_user.email,
+         password: new_user.password
+      }),
+      success: function(response) {
+         console.log('Success:', response);
+         new_user.id = response.id;
+         user_list.push(new_user);
+         $(".container-fluid").html($("#view-home").html())
+      },
+      error: function(xhr, status, error) {
+         console.error('Error:', error);
+         alert("User already exists")
+      }
+   });
+}
+
+function login(email, password) {
+   $.ajax({
+      url: getHost() + '/login',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+         email: email,
+         password: password
+      }),
+      success: function(response) {
+         console.log('Success:', response);
+         $(".container-fluid").html($("#view-home").html())
+         loadCarList();
+      },
+      error: function(xhr, status, error) {
+         console.error('Error:', error);
+         alert("Email or password is incorrect")
+      }
+   });
+}
+
 $('.nav-link').click(function (e) {
    e.preventDefault();
 
@@ -119,6 +163,10 @@ $('.nav-link').click(function (e) {
    } else if (view === "cars") {
       $(".container-fluid").html($("#view-cars").html())
       loadCarList();
+   } else if (view === "sign-up") {
+      $(".container-fluid").html($("#view-sign-up").html())
+   } else if (view === "login") {
+      $(".container-fluid").html($("#view-login").html())
    }
 });
 
@@ -128,7 +176,7 @@ $(document).on("click", ".send-button", function (e) {
    const message = $("#message").val()
 
    if (!name | !email | !message) {
-      alert("Fill in all fields")
+      alert("Fill in all f1ields")
       return;
    }
    alert('Name: '+name+'\nEmail: '+email+'\nMessage: '+message);
@@ -144,32 +192,32 @@ $(document).on("click", ".save-add-button", function (e) {
    const input_name = $("#name").val()
    const input_email = $("#email").val()
    
-   let added_car = {make: '', model: '', customer: {}}
+   let added_car = {make: '', model: '', user: {}}
    
    if (!(input_make && input_model)) {
       alert("Make and model need to be filled.")
       return;
    } else if (input_name || input_email) {
       if (!(input_name && input_email)) {
-         alert("You need to write both NAME and EMAIL to add a customer")
+         alert("You need to write both NAME and EMAIL to add a user")
          return;
       } else {
-         var customer_match = null
+         var user_match = null
          
-         for (let i = 0; i < customer_list.length; i++) {
-            if (customer_list[i].name == input_name && customer_list[i].email == input_email) {
-               customer_match = customer_list[i]
-            } else if (customer_list[i].email == input_email) {
+         for (let i = 0; i < user_list.length; i++) {
+            if (user_list[i].name == input_name && user_list[i].email == input_email) {
+               user_match = user_list[i]
+            } else if (user_list[i].email == input_email) {
                alert("Error. Either the email is already taker or you have spelled the name wrong.")
                return;
             }
          }
-         if (customer_match) {
+         if (user_match) {
             added_car.make = input_make;
             added_car.model = input_model;
-            added_car.customer = getCustomerFromId(customer_match.id);
+            added_car.user = getUserFromId(user_match.id);
          } else {
-            alert("Customer doesn't exist")
+            alert("User doesn't exist")
             return;
          }
       }
@@ -195,37 +243,37 @@ $(document).on("click", ".save-edit-button", function (e) {
       const car = car_not_formatted[0]
       const input_make = $("#make").val()
       const input_model = $("#model").val()
-      const input_customer = $("#customer").val()
+      const input_user = $("#user").val()
 
-      if (input_customer) {
-         var input_customer_id = NaN
+      if (input_user) {
+         var input_user_id = NaN
 
-         customer_list.forEach(customer => {
-            if (customer.name == input_customer) {
-               input_customer_id = customer.id
+         user_list.forEach(user => {
+            if (user.name == input_user) {
+               input_user_id = user.id
                console.log("match cust")
             }
          });
 
-         if (!input_customer_id) {
-            alert("No customer with that name exists")
+         if (!input_user_id) {
+            alert("No user with that name exists")
          }
       }
 
-      if (input_make && input_model && input_customer_id) {
-         updateCar(carId, input_make, input_model, input_customer_id)
-      } else if (input_model && input_customer_id) {
-         updateCar(carId, car.make, input_model,input_customer_id)
-      } else if (input_make && input_customer_id) {
-         updateCar(carId, input_make, car.model, input_customer_id)
+      if (input_make && input_model && input_user_id) {
+         updateCar(carId, input_make, input_model, input_user_id)
+      } else if (input_model && input_user_id) {
+         updateCar(carId, car.make, input_model,input_user_id)
+      } else if (input_make && input_user_id) {
+         updateCar(carId, input_make, car.model, input_user_id)
       } else if (input_make && input_model) {
-         updateCar(carId, input_make, input_model, car.customer ? car.customer.id : 0)
+         updateCar(carId, input_make, input_model, car.user ? car.user.id : 0)
       }  else if (input_make) {
-         updateCar(carId, input_make, car.model, car.customer ? car.customer.id : 0)
+         updateCar(carId, input_make, car.model, car.user ? car.user.id : 0)
       } else if (input_model) {
-         updateCar(carId, car.make, input_model, car.customer ? car.customer.id : 0)
-      } else if (input_customer_id) {
-         updateCar(carId, car.make, car.model, input_customer_id)
+         updateCar(carId, car.make, input_model, car.user ? car.user.id : 0)
+      } else if (input_user_id) {
+         updateCar(carId, car.make, car.model, input_user_id)
       }
    })
 });
@@ -235,8 +283,39 @@ $(document).on("click", ".delete-button", function (e) {
    deleteCar(carId)
 });
 
-function getCustomerFromId(id) {
-   return customer_list.find(customer => customer.id == id);
+$(document).on("click", ".sign-up-button", function (e) {
+   const name = $("#name").val()
+   const email = $("#email").val()
+   const password = $("#password").val()
+
+   if (!name | !email | !password) {
+      alert("Fill in all fields")
+      return;
+   }
+   const new_user = {
+      name: name,
+      email: email,
+      password: password
+   }
+   signUp(new_user);
+});
+
+
+$(document).on("click", ".login-button", function (e) {
+   const email = $("#email").val()
+   const password = $("#password").val()
+
+   if (!email | !password) {
+      alert("Fill in all fields")
+      return;
+   }
+   
+   login(email, password);
+});
+
+
+function getUserFromId(id) {
+   return user_list.find(user => user.id == id);
 }
 
 function getCarFromId(id) {
@@ -248,13 +327,13 @@ function loadCarList(carId = 0) {
    $("#show-cars").empty()
    var car_num = 1
    car_list.forEach(car => {
-      const car_owner = car.customer?.name ?? "No customer";
+      const car_owner = car.user?.name ?? "No user";
       const carHtml = `
          <div>
             <p><strong>Car ${car_num}</strong></p>
             <p>Make: ${car.make}</p>
             <p>Model: ${car.model}</p>
-            <p>Customer: ${car_owner}</p>
+            <p>User: ${car_owner}</p>
             <button id="editButton${car.id}" class="edit-button" data-toggle="modal" data-target="#editModal">EDIT</button>
             <button id="deleteButton${car.id}" class="delete-button">DELETE</button>
             <br><br>
@@ -309,7 +388,7 @@ function createModal(modalType, car = {}) {
       `;
 
    } else if (modalType === "edit" && car) {
-      const car_owner = car.customer?.name ?? "No customer";
+      const car_owner = car.user?.name ?? "No user";
       modalHtml = `
          <div class="modal" id="editModal" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
@@ -330,8 +409,8 @@ function createModal(modalType, car = {}) {
                         <input type="text" id="model" class="car-info-input" placeholder="${car.model}">
                      </div>
                      <div class="horisontial">
-                        <p>Customer: &nbsp;</p>
-                        <input type="text" id="customer" class="car-info-input" placeholder="${car_owner}">
+                        <p>User: &nbsp;</p>
+                        <input type="text" id="user" class="car-info-input" placeholder="${car_owner}">
                      </div>
                   </div>
                   <div class="modal-footer">
--- server/main.py
+++ server/main.py
@@ -2,6 +2,8 @@
 from flask import Flask, jsonify, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt, generate_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 
 app = Flask(__name__, static_folder='../client', static_url_path='/')
 
@@ -9,45 +11,61 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'mYEM3e44ZZ58emfNwawOrf6ux0fy5o'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     make = db.Column(db.String, nullable=False)
     model = db.Column(db.String, nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
 
     def serialize(self):
-        if self.customer_id:
-            customer = Customer.query.get(self.customer_id)
+        if self.user_id:
+            user = User.query.get(self.user_id)
             return dict(
                 id=self.id,
                 make=self.make,
                 model=self.model,
-                customer=customer.serialize() if customer else None
+                user=user.serialize() if user else None
             )
         else:
             return dict(
                 id=self.id,
                 make=self.make,
                 model=self.model,
-                customer=None
+                user=None
             )
 
 
-class Customer(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    cars = db.relationship('Car', backref='customer', lazy=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default = False)
+    cars = db.relationship('Car', backref='user', lazy=True)
 
     def serialize(self):
         return dict(
             id=self.id,
             name=self.name,
-            email=self.email
+            email=self.email,
+            is_admin=self.is_admin
         )
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password).decode('utf8')
+    
+    def checkPassword(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
 
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
 def get_or_abort(model, record_id):
     record = model.query.get(record_id)
@@ -57,6 +75,7 @@ def get_or_abort(model, record_id):
 
 
 @app.route('/cars', methods=['GET', 'POST'])
+@jwt_required()
 def cars():
     if request.method == 'GET':
         car_list = Car.query.all()
@@ -66,18 +85,19 @@ def cars():
         added_car = request.get_json(force=True)
         new_make = added_car.get('make')
         new_model = added_car.get('model')
-        new_customer_id = added_car.get('customer')
+        new_user_id = added_car.get('user')
 
         if not new_make or not new_model:  # Ensure make and model are provided
             abort(400, description="Make and model are required fields.")
         
-        new_car = Car(make=new_make, model=new_model, customer_id=new_customer_id if new_customer_id else None)
+        new_car = Car(make=new_make, model=new_model, user_id=new_user_id if new_user_id else None)
         db.session.add(new_car)
         db.session.commit()
         return jsonify(new_car.serialize()), 201
 
 
 @app.route('/cars/<int:car_id>',methods=['GET', 'PUT', 'DELETE'])
+@jwt_required()
 def show_car_id(car_id):
     car = get_or_abort(Car, car_id)
 
@@ -90,9 +110,9 @@ def show_car_id(car_id):
             car.make = data['make']
         if "model" in data :
             car.model = data['model']
-        if "customer_id" in data and data["customer_id"] != 0:
-                customer = get_or_abort(Customer, data['customer_id'])
-                car.customer_id = customer.id
+        if "user_id" in data and data["user_id"] != 0:
+                user = get_or_abort(User, data['user_id'])
+                car.user_id = user.id
         
         db.session.commit()
         return jsonify([car.serialize()])
@@ -103,55 +123,106 @@ def show_car_id(car_id):
         return '', 200
 
 
-@app.route('/customers', methods=['GET', 'POST'])
-def customer():
+@app.route('/users', methods=['GET', 'POST'])
+@jwt_required()
+def user():
     if request.method == 'GET':
-        customer_list = Customer.query.all()
-        return jsonify([customer.serialize() for customer in customer_list]), 200
+        user_list = User.query.all()
+        return jsonify([user.serialize() for user in user_list]), 200
 
     elif request.method == 'POST':
-        added_customer = request.get_json(force=True)
-        new_name = added_customer.get('name')
-        new_email = added_customer.get('email')
+        added_user = request.get_json(force=True)
+        new_name = added_user.get('name')
+        new_email = added_user.get('email')
+        new_is_admin = added_user.get('is_admin', False)
 
         if not new_name or not new_email:  # Ensure name and email are provided
             abort(400, description="Name and email are required fields.")
 
-        new_customer = Customer(name=new_name, email=new_email)
-        db.session.add(new_customer)
+        new_user = User(name=new_name, email=new_email, is_admin=new_is_admin)
+        db.session.add(new_user)
         db.session.commit()
-        return jsonify({"message": f"New customer added. Name: {new_name}, Email: {new_email}"}), 201
+        return jsonify({"message": f"New user added. Name: {new_name}, Email: {new_email}"}), 201
 
 
-@app.route('/customers/<int:customer_id>',methods=['GET', 'PUT', 'DELETE'])
-def show_customer_id(customer_id):
-    customer = get_or_abort(Customer, customer_id)
+@app.route('/users/<int:user_id>',methods=['GET', 'PUT', 'DELETE'])
+@jwt_required()
+def show_user_id(user_id):
+    user = get_or_abort(User, user_id)
 
     if request.method == 'GET':
-        return jsonify([customer.serialize()])
+        return jsonify([user.serialize()])
 
     elif request.method == 'PUT':
         data  = request.get_json(force=True)
         if "name" in data : 
-            customer.name = data['name']
+            user.name = data['name']
         if "email" in data : 
-            customer.email = data['email']
+            user.email = data['email']
         db.session.commit()
-        return jsonify([customer.serialize()])
+        return jsonify([user.serialize()])
 
     elif request.method == 'DELETE':
-        for car in customer.cars :
-            car.customer_id = None
+        for car in user.cars :
+            car.user_id = None
         
-        db.session.deslete(customer)
+        db.session.deslete(user)
         db.session.commit()
         return '', 200
 
 
-@app.route('/customers/<int:customer_id>/cars',methods=['GET'])
-def show_customers_cars(customer_id):
-    customer = get_or_abort(Customer, customer_id)
-    return jsonify([car.serialize() for car in customer.cars])
+@app.route('/users/<int:user_id>/cars',methods=['GET'])
+@jwt_required()
+def show_users_cars(user_id):
+    user = get_or_abort(User, user_id)
+    return jsonify([car.serialize() for car in user.cars])
+
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+   if request.method == 'POST':
+        data = request.get_json(force=True)
+        new_name = data.get('name')
+        new_email = data.get('email')
+        new_is_admin = data.get('is_admin', False)
+
+        if not new_name or not new_email or not data.get('password'):
+            abort(400, description="Name, email and password are required fields.")
+
+        if User.query.filter_by(email=new_email).first():
+            return jsonify({"error": "User already exists"}), 400
+
+        new_user = User(name=new_name, email=new_email, is_admin=new_is_admin)
+        new_user.set_password(data.get('password'))
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"message": f"New user added. Name: {new_name}, Email: {new_email}"}), 201
+
+
+@app.route("/login", methods=['POST'])
+def login():
+   if request.method == 'POST':
+        data = request.get_json(force=True)
+        email = data.get('email')
+        password = data.get('password')
+
+        if not email or not password:
+            abort(400, description="Email and password are required fields.")
+
+        user = User.query.filter_by(email=email).first()
+        
+        if not user:
+            return jsonify({"error": "User doesn't exist"}), 401
+        elif not user.checkPassword(password):
+            return jsonify({"error": "Invalid password"}), 401
+
+        token = create_access_token(identity=str(user.id))
+
+        return jsonify({
+            "token": token,
+            "user": user.serialize()
+            }), 200
 
 
 @app.route("/")
